dee63df818ee16986ad252ee97fbe9575421d3bd
Kiner Shah <kiner.shah1@gmail.com>
2019-09-19 20:15:31 +0530

    Create grid_challenge_1.java

---
+++ Hackerrank/Magic Lines September 2015/grid_challenge_1.java
@@ -0,0 +1,159 @@
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.io.BufferedWriter;
+import java.util.InputMismatchException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.math.BigInteger;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+   public static void main(String[] args) {
+      InputStream inputStream = System.in;
+      OutputStream outputStream = System.out;
+      InputReader in = new InputReader(inputStream);
+      OutputWriter out = new OutputWriter(outputStream);
+      GridChallenge solver = new GridChallenge();
+      int testCount = Integer.parseInt(in.next());
+      for (int i = 1; i <= testCount; i++)
+         solver.solve(i, in, out);
+      out.close();
+   }
+}
+
+class GridChallenge {
+    public void solve(int testNumber, InputReader in, OutputWriter out) {
+        int n = in.readInt();
+        char[][] g = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            g[i] = in.readString().toCharArray();
+            Arrays.sort(g[i]);
+        }
+        boolean ok = true;
+        for (int j = 0; j<n; j++) {
+            for (int i = 0; i < n - 1; i++) {
+                if (g[i + 1][j] < g[i][j]) {
+                    ok = false;
+                    break;
+                }
+            }
+        }
+        if (ok) {
+            out.printLine("YES");
+        }
+        else {
+            out.printLine("NO");
+        }
+    }
+}
+
+class InputReader {
+
+    private InputStream stream;
+    private byte[] buf = new byte[1024];
+    private int curChar;
+    private int numChars;
+    private SpaceCharFilter filter;
+
+    public InputReader(InputStream stream) {
+        this.stream = stream;
+    }
+
+    public int read() {
+        if (numChars == -1)
+            throw new InputMismatchException();
+        if (curChar >= numChars) {
+            curChar = 0;
+            try {
+                numChars = stream.read(buf);
+            } catch (IOException e) {
+                throw new InputMismatchException();
+            }
+            if (numChars <= 0)
+                return -1;
+        }
+        return buf[curChar++];
+    }
+
+    public int readInt() {
+        int c = read();
+        while (isSpaceChar(c))
+            c = read();
+        int sgn = 1;
+        if (c == '-') {
+            sgn = -1;
+            c = read();
+        }
+        int res = 0;
+        do {
+            if (c < '0' || c > '9')
+                throw new InputMismatchException();
+            res *= 10;
+            res += c - '0';
+            c = read();
+        } while (!isSpaceChar(c));
+        return res * sgn;
+    }
+
+    public String readString() {
+        int c = read();
+        while (isSpaceChar(c))
+            c = read();
+        StringBuilder res = new StringBuilder();
+        do {
+            res.appendCodePoint(c);
+            c = read();
+        } while (!isSpaceChar(c));
+        return res.toString();
+    }
+
+    public boolean isSpaceChar(int c) {
+        if (filter != null)
+            return filter.isSpaceChar(c);
+        return isWhitespace(c);
+    }
+
+    public static boolean isWhitespace(int c) {
+        return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+    }
+
+    public String next() {
+        return readString();
+    }
+
+    public interface SpaceCharFilter {
+        public boolean isSpaceChar(int ch);
+    }
+}
+
+class OutputWriter {
+    private final PrintWriter writer;
+
+    public OutputWriter(OutputStream outputStream) {
+        writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+    }
+
+    public void print(Object...objects) {
+        for (int i = 0; i < objects.length; i++) {
+            if (i != 0)
+                writer.print(' ');
+            writer.print(objects[i]);
+        }
+    }
+
+    public void printLine(Object...objects) {
+        print(objects);
+        writer.println();
+    }
+
+    public void close() {
+        writer.close();
+    }
+
+}
