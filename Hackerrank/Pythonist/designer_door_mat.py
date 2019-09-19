4714143d4abf562a21ca0712a55653e9bb5856f2
Kiner Shah <kiner.shah1@gmail.com>
2019-09-19 20:20:52 +0530

    Create designer_door_mat.py

---
+++ Hackerrank/Pythonist/designer_door_mat.py
@@ -0,0 +1,6 @@
+N, M = map(int,raw_input().split()) # More than 6 lines of code will result in 0 score. Blank lines are not counted.
+for i in xrange(1,N,2): 
+    print "-"*((M-3*i)/2)+".|."*i+"-"*((M-3*i)/2)
+print "-"*((M-7)/2)+"WELCOME"+"-"*((M-7)/2)
+for i in xrange(N-2,-1,-2): 
+    print "-"*((M-3*i)/2)+".|."*i+"-"*((M-3*i)/2)
